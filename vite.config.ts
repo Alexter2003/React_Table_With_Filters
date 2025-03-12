f27c7ffde9ba277d9b21a35390c743ac7bf0ed12
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-03-12 09:30:24 -0600

    change base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'; // Importa el módulo 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/React_Table_With_Filters/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Configura el alias manualmente
